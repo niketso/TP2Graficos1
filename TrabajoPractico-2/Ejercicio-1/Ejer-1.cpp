620a7aaedded802b3daa0dcda551960bf279933b
Unknown <nicolaspiccitto@gmail.com>
2018-04-24 17:57:40 -0300

    TP2 V2
    Agrego sprite y movimiento

--- TrabajoPractico-2/Ejercicio-1/Ejer-1.cpp
+++ TrabajoPractico-2/Ejercicio-1/Ejer-1.cpp
@@ -1,24 +1,53 @@
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
+void Movement(sf::Sprite &sprite,int speed,float Tiempo) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+		sprite.move(-speed*Tiempo, 0);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+		sprite.move(speed*Tiempo, 0);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
+		sprite.move(0,-speed*Tiempo);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) 
+		sprite.move(0,speed*Tiempo);
+}
+
+
+int main() {
+	sf::RenderWindow window;
+	sf::Texture texture;
+	int speed = 30;
+	
+	window.create(sf::VideoMode(800, 600), "Ejemplo 1");
+	if (!texture.loadFromFile("Assets/Azul.png"))
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+		cout << "no cargo la textura " << endl;
+	}
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sf::Clock clock;
+
+	while (window.isOpen()){		
+		
 
 		window.clear();
-		window.draw(shape);
+		window.draw(sprite);
 		window.display();
-	}
 
-	return 0;
+		sf::Time elapsed = clock.restart();
+		Movement(sprite, speed, elapsed.asSeconds());
+
+		sf::Event event;
+	while (window.pollEvent(event)){						
+			if (event.type == sf::Event::Closed)
+				window.close();
+			
+		}
+	}
+		return 0;
 }
+
+	
